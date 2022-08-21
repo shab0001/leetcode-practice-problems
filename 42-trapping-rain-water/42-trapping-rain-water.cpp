c6438002e6c9f096799f704ea5c2c4d82025bace
shab0001 <76178893+shab0001@users.noreply.github.com>
2022-08-21 13:54:01 +0530

    Time: 18 ms (82.77%), Space: 20.4 MB (36.61%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
-   int trap(vector<int>& height)
-{
-	int leftmax = INT_MIN;
-	int rightmax = INT_MIN, ans = 0;
-	int i = 0, j = height.size() - 1;
-	while (i < j)
-	{
-		if (height[i] < height[j])
-		{
-			if (height[i] > leftmax)
-			{
-				leftmax = max(leftmax, height[i]);
-			}
-			else {
-				ans += (leftmax - height[i]);
-			}
-			i++;
-		}
-		else
-		{
-			if (height[j] > rightmax)
-			{
-				rightmax = max(rightmax, height[j]);
-			}
-			else
-			{
-				ans += (rightmax - height[j]);
-			}
-			j--;
-		}
-	}
-       return ans;
-   }
-    
+    int trap(vector<int>& height) 
+    {
+        stack<int>st;
+        int total_water =0;
+        int current=0;
+        while(current<height.size())
+        {
+            while(!st.empty() and height[current]> height[st.top()])
+            {
+                int top=st.top();
+                st.pop();
+                if(st.empty())
+                {
+                    break;
+                }
+                int x=current-st.top()-1;
+                int y=min(height[st.top()],height[current])-height[top];
+                total_water+=x*y;
+            }
+            st.push(current);
+            current++;
+        }
+        return total_water;
+    }
 };
