0e7d3fbccb64c2117c97c211d0c7c991c2f287d0
shab0001 <76178893+shab0001@users.noreply.github.com>
2022-09-01 16:36:15 +0530

    Time: 117 ms (5.52%), Space: 36.2 MB (5.38%) - LeetHub

--- 561-array-partition/561-array-partition.cpp
+++ 561-array-partition/561-array-partition.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-    int arrayPairSum(vector<int>& nums) {
-        int n=size(nums);
-        sort(nums.begin(),nums.end());
-        int sum=0,minpair;
-  for(int i=0;i<n;i+=2)
-  {
-minpair=min(nums[i],nums[i+1]);
-sum+=minpair;
-  }
-  return sum;
+    int arrayPairSum(vector<int>& v) {
+        int left = -10000;
+        int right = 10000;
+        vector<int>freq(right - left + 1, 0);
+        for (auto x : v) {
+            freq[x - left]++;
+        }
+        int ans = 0, prev = 0;
+        for (int i = left ; i <= right ; i++) {
+            ans += ((freq[i - left] + 1 - prev) / 2) * i;
+            prev = (prev + freq[i - left]) % 2;
+        }
+        return ans;
     }
 };
